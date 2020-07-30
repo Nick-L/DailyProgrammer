3e8f5096c174f27378e7dff083ba939b96d0558a
Nick Lang <nslang18@gmail.com>
2020-07-30 09:38:35 -0500

    Implemented first iteration of game without island checking and test cases for the class

--- DailyProgrammer/Intermediate/Challenge_375/Game.cs
+++ DailyProgrammer/Intermediate/Challenge_375/Game.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace DailyProgrammer.Intermediate.Challenge_375
 {
     public class Game
     {
+        private List<Card> Cards;
+        private int LastCardIndex;
+
+        public Game(string initialGameState)
+        {
+            Cards = new List<Card>();
+            LastCardIndex = initialGameState.Length - 1;
+            foreach(char card in initialGameState)
+            {
+                Cards.Add(new Card(card));
+            }
+        }
+
+        public void RemoveCard(int index)
+        {
+            if(index < 0 || index > LastCardIndex)
+            {
+                throw new IndexOutOfRangeException($"Error: Attempted to flip card outside of range index was {index}");
+            }
+            else if(Cards[index].State != CardState.FACE_UP)
+            {
+                throw new InvalidOperationException($"Error: Cannot remove a card that is not face up card was {Cards[index].State}");
+            }
+            if(index == 0)
+            {
+                Cards[0].FlipCard();
+                Cards[1].FlipCard();
+            }
+            else if(index == LastCardIndex)
+            {
+                Cards[LastCardIndex - 1].FlipCard();
+                Cards[LastCardIndex].FlipCard();
+            }
+            else
+            {
+                Cards[index - 1].FlipCard();
+                Cards[index].FlipCard();
+                Cards[index + 1].FlipCard();
+            }
+        }
+
+        public bool IslandExists()
+        {
+            return false;
+        }
+
+        public string CurrentGameState()
+        {
+            StringBuilder sb = new StringBuilder(Cards.Count);
+            Char cardValue;
+            foreach(Card card in Cards)
+            {
+                switch (card.State)
+                {
+                    case CardState.FACE_UP:
+                        cardValue = '1';
+                        break;
+                    case CardState.FACE_DOWN:
+                        cardValue = '0';
+                        break;
+                    case CardState.REMOVED:
+                        cardValue = '.';
+                        break;
+                    default:
+                        throw new Exception($"Error: Unexpected Value for card.State was {card.State}");
+                }
+                sb.Append(cardValue);
+            }
+
+            return sb.ToString();
+        }
     }
 }
---
+++ DailyProgrammerTests/Intermediate/Challenge_375/Game_Tests.cs
@@ -0,0 +1,69 @@
+﻿using DailyProgrammer.Intermediate.Challenge_375;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyProgrammerTests.Intermediate.Challenge_375
+{
+    [TestClass]
+    public class Game_Tests
+    {
+        private Game game;
+
+        private const string INPUT_WITHOUT_ISLANDS = "1.10.010.101.";
+        private const string INPUT_WITH_ISLANDS = "1.10.000.101.";
+
+        [TestMethod]
+        public void CardsHaveCorrectState()
+        {
+            CreateGame_WithoutIslands();
+
+            Assert.AreEqual(INPUT_WITHOUT_ISLANDS, game.CurrentGameState());
+
+            CreateGame_WithIslands();
+
+            Assert.AreEqual(INPUT_WITH_ISLANDS, game.CurrentGameState());
+        }
+
+        [TestMethod]
+        public void CardsHaveCorrectStateAfterRemovingCard()
+        {
+            CreateGame_WithoutIslands();
+
+            game.RemoveCard(0);
+
+            Assert.AreEqual("..10.010.101.", game.CurrentGameState());
+
+            game.RemoveCard(6);
+
+            Assert.AreEqual("..10.1.1.101.", game.CurrentGameState());
+        }
+        [TestMethod]
+        public void GameFailsToCreateWithInvalidInput()
+        {
+            Assert.ThrowsException<Exception>(() => { new Game(".01c"); });
+        }
+        
+        [TestMethod]
+        public void FailsToRemoveCardAtInvalidIndex()
+        {
+            CreateGame_WithIslands();
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => { game.RemoveCard(-1); });
+            Assert.ThrowsException<IndexOutOfRangeException>(() => { game.RemoveCard(INPUT_WITH_ISLANDS.Length); });
+            Assert.ThrowsException<InvalidOperationException>(() => { game.RemoveCard(INPUT_WITH_ISLANDS.IndexOf('0')); });
+            Assert.ThrowsException<InvalidOperationException>(() => { game.RemoveCard(INPUT_WITH_ISLANDS.IndexOf('.')); });
+        }
+
+        private void CreateGame_WithoutIslands()
+        {
+            game = new Game(INPUT_WITHOUT_ISLANDS);
+        }
+
+        private void CreateGame_WithIslands()
+        {
+            game = new Game(INPUT_WITH_ISLANDS);
+        }
+    }
+}
